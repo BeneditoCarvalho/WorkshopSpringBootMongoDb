b7cbcc8b6cf85c0a80c87b89b5a70a0f9969e7ee
Benedito Carvalho <benedito.carvalho@gmail.com>
2021-08-28 11:56:07 -0300

    Atualização de usuário com PUT

--- src/main/java/com/logos/workshopmongo/services/UserService.java
+++ src/main/java/com/logos/workshopmongo/services/UserService.java
@@ -25,17 +25,28 @@ public class UserService {
 		Optional<User> user = repository.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
-	
+
 	public User insert(User obj) {
 		return repository.insert(obj);
 	}
-	
-	public void delete (String id) {
+
+	public void delete(String id) {
 		findById(id);
 		repository.deleteById(id);
 	}
-	
-	public User fromDTO (UserDTO objDTO) {
+
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repository.save(newObj);
+	}
+
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+	}
+
+	public User fromDTO(UserDTO objDTO) {
 		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
 
